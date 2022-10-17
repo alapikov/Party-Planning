ae23be884527688d0bba85941ed445223c986dcf
Alexander Lapikov <racman996@gmail.com>
2022-10-17 22:29:23 +0300

    project created

---
+++ webpack.config.js
@@ -0,0 +1,112 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'none',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    target: 'web',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: '/node_modules/'
+            }
+        ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const webpack = require("webpack");
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// const production = process.env.NODE_ENV === 'production';
+
+
+// module.exports = {
+//     entry: { myAppName: path.resolve(__dirname, "./src/index.js") },
+//     output: {
+//         path: path.resolve(__dirname, "./dist"),
+//         filename: production ? '[name].[contenthash].js' : '[name].js',
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.(js|jsx)$/,
+//                 exclude: /node_modules/,
+//                 use: ["babel-loader"],
+//             },
+//             {
+//                 test: /\.s(a|c)ss$/,
+//                 exclude: /node_modules/,
+//                 use: [
+//                     production ? MiniCssExtractPlugin.loader : 'style-loader',
+//                     {
+//                         loader: 'css-loader',
+//                         options: {
+//                             modules: true,
+//                             sourceMap: !production
+//                         }
+//                     },
+//                     {
+//                         loader: 'sass-loader',
+//                         options: {
+//                             sourceMap: !production
+//                         }
+//                     }
+//                 ]
+//             },
+
+//         ],
+//     },
+//     resolve: {
+//         extensions: ["*", ".js", ".jsx", ".scss"],
+//     },
+//     plugins: [
+//         new CleanWebpackPlugin(),
+//         new webpack.HotModuleReplacementPlugin(),
+//         new HtmlWebpackPlugin({
+//             title: "Webpack & React",
+//             template: "./src/index.html",
+//             favicon: "./public/favicon.ico"
+//         }),
+//         new MiniCssExtractPlugin({
+//             filename: production ? '[name].[contenthash].css' : '[name].css',
+//         }),
+//     ],
+//     devServer: {
+//         port: 3001,
+//         hot: true,
+//     },
+//     mode: production ? 'production' : 'development'
+// };
